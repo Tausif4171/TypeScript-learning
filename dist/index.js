8ad06e9533cfa5341652f948c00be759f7042f53
Tausif4171 <tausifkhan4171@gmail.com>
2022-04-16 02:08:57 +0530

    Array and Tuples

--- dist/index.js
+++ dist/index.js
@@ -8,3 +8,18 @@ let Age;
 Age = 22;
 let x = 'string'; // Here we have store a string value to 'x' variable, we can store anything boolean, number, etc. 
 x = 12; // Here we have store a number value to 'x'
+// Array
+let person = [1, 2, 3, 4, 5];
+// person.push("Rehan")  // we cannot push string to number array
+let arr = [1, 'Faizan', true];
+// Tuple
+let data = [29, 'Tausif Khan', true];
+// console.log(data[0])
+// Tuple Array 
+let info;
+info = [
+    [10, 'mango'],
+    [20, 'apple'],
+    [30, 'grapes']
+];
+console.log(info);
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,27 @@ Age = 22
 
 let x: any = 'string'  // Here we have store a string value to 'x' variable, we can store anything boolean, number, etc. 
 x = 12    // Here we have store a number value to 'x'
+
+// Array
+
+let person: number[] = [1,2,3,4,5]
+// person.push("Rehan")  // we cannot push string to number array
+
+let arr: any[] = [1,'Faizan',true ]
+
+// Tuple
+
+let data: [number,string,boolean] = [29,'Tausif Khan',true]
+// console.log(data[0])
+
+// Tuple Array 
+
+let info: [number,string][]
+
+info = [
+    [10,'mango'],
+    [20,'apple'],
+    [30,'grapes']
+]
+
+console.log(info)
