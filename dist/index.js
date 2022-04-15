545d5b9e8f5cd932cd72fba149fe1b0e222f057b
Tausif4171 <tausifkhan4171@gmail.com>
2022-04-16 01:48:01 +0530

    Basic Types of TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-let id = 5;
-console.log("Id: ", id);
+// Basic Types
+let Name = 'Tausif Khan'; // Here if we stored number or boolean or any type it throws error,
+// because we have given the type string to variable 'Name'
+let Rollno = 29;
+let isPresent = true;
+let Age;
+Age = 22;
+let x = 'string'; // Here we have store a string value to 'x' variable, we can store anything boolean, number, etc. 
+x = 12; // Here we have store a number value to 'x'
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-let id:number =5
+// Basic Types
 
-console.log("Id: ", id)
+let Name: string = 'Tausif Khan'  // Here if we stored number or boolean or any type it throws error,
+                                  // because we have given the type string to variable 'Name'
+let Rollno: number = 29
+
+let isPresent: boolean = true
+
+let Age: number
+Age = 22
+
+let x: any = 'string'  // Here we have store a string value to 'x' variable, we can store anything boolean, number, etc. 
+x = 12    // Here we have store a number value to 'x'
