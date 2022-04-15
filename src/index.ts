eb346b19d1c75835a7774f91ad658f4b2a73afde
Tausif4171 <tausifkhan4171@gmail.com>
2022-04-16 02:48:48 +0530

    given comment to object

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Basic Types
 
 let Name: string = 'Tausif Khan'  // Here if we stored number or boolean or any type it throws error,
-                                  // because we have given the type string to variable 'Name'
+// because we have given the type string to variable 'Name'
 let Rollno: number = 29
 
 let isPresent: boolean = true
@@ -14,24 +14,24 @@ x = 12    // Here we have store a number value to 'x'
 
 // Array
 
-let person: number[] = [1,2,3,4,5]
+let person: number[] = [1, 2, 3, 4, 5]
 // person.push("Rehan")  // we cannot push string to number array
 
-let arr: any[] = [1,'Faizan',true ]
+let arr: any[] = [1, 'Faizan', true]
 
 // Tuple - Tuple can contain two or more values of different data types.
 
-let data: [number,string,boolean] = [29,'Tausif Khan',true]
+let data: [number, string, boolean] = [29, 'Tausif Khan', true]
 // console.log(data[0])
 
 // Tuple Array 
 
-let fruits: [number,string][]
+let fruits: [number, string][]
 
 fruits = [
-    [10,'mango'],
-    [20,'apple'],
-    [30,'grapes']
+    [10, 'mango'],
+    [20, 'apple'],
+    [30, 'grapes']
 ]
 
 console.log(fruits)
@@ -39,14 +39,14 @@ console.log(fruits)
 // Union - a particular variable to be able to hold more than one type
 
 let pId: number | string = 2
-pId='hello'
+pId = 'hello'
 console.log(pId)
 
 // Enum - Enums allow a developer to define a set of named constants.
 
 enum Direction1 { // Here top is initialized with 5. All of the following members are auto-incremented from that point on. 
-                  // In other words, Direction.top has the value 5, bottom has 6, left has 7, and right has 8.
-    top=5,
+    // In other words, Direction.top has the value 5, bottom has 6, left has 7, and right has 8.
+    top = 5,
     bottom,
     left,
     right
@@ -54,23 +54,23 @@ enum Direction1 { // Here top is initialized with 5. All of the following member
 console.log(Direction1.right)
 
 enum Direction2 {
-    top="john",
-    bottom="doe",
-    left="lily",
-    right='zayn'
+    top = "john",
+    bottom = "doe",
+    left = "lily",
+    right = 'zayn'
 }
 console.log(Direction2.top)
 
 // Objects
 
-type user ={
+type user = {   // Here we have given the object types, like object has to be number and string
     id: number,
     name: string
 }
 
-let info : user = {
-    id:1,
-    name:'Bob'
+let info: user = {
+    id: 1,
+    name: 'Bob'
 }
 
 // console.log(info.name)
